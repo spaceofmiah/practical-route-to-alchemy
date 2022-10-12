7676ede30acc6ecec9b3b411abed7173fcf693d7
spaceofmiah <agbonzeosazuwa@gmail.com>
2022-10-12 14:58:00 +0100

    Import raw db queries & run create_item_table

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from src.basic import run_db_select_statement
+from db.scripts.queries import (
+    create_item_table, 
+    drop_item_table, 
 
+    create_craved_item_table,
+    drop_craved_item_table
+)
 
 if __name__ == "__main__":
-    run_db_select_statement()
+    create_item_table()
         
