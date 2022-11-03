2c2013955795a2d75414e9ea72513af8a978ffff
spaceofmiah <agbonzeosazuwa@gmail.com>
2022-11-03 20:58:02 +0100

    Update import and function & method calls

--- main.py
+++ main.py
@@ -1,35 +1,17 @@
 from src.basic import run_db_select_statement
-from db.scripts.queries import (
-    create_item_table, 
-    drop_item_table, 
-    show_all_tables,
-    rename_item_table,
-    insert_item,
-    delete_item,
-    retrieve_all_item,
-    insert_multiple_items,
-    update_existing_item_name,
-
-    create_craved_item_table,
-    drop_craved_item_table
-)
-from db.models.items import (
-    create_all_tables,
-    drop_all_tables,
-    drop_craveditem,
-    drop_item,
-)
+from db.scripts import queries as q
+from db.scripts import items_el
 
 if __name__ == "__main__":
-    # create_item_table()
-    # create_craved_item_table()
-    # show_all_tables()
-    # rename_item_table('Commodity', 'Item')
-    # drop_item_table()
-    # drop_craved_item_table()
-    # show_all_tables()
-    # insert_item(name="Tesla Model S", category="Auto")
-    # insert_multiple_items([
+    # q.create_item_table()
+    # q.create_craved_item_table()
+    # q.show_all_tables()
+    # q.rename_item_table('Commodity', 'Item')
+    # q.drop_item_table()
+    # q.drop_craved_item_table()
+    # q.show_all_tables()
+    # q.insert_item(name="Tesla Model S", category="Auto")
+    # q.insert_multiple_items([
     #     {
     #         'name': 'Iphone 14 Pro Max',   
     #         'category': 'mobile'
@@ -39,13 +21,13 @@ if __name__ == "__main__":
     #         'category': 'meal'
     #     }
     # ])
-    # update_existing_item_name(item_id=1, new_value="Tesla Starlink")
-    # delete_item(1)
-    # retrieve_all_item()
-    # create_all_tables()
-    # drop_all_tables()
-    drop_craveditem()
-    drop_item()
-    show_all_tables()
+    # q.update_existing_item_name(item_id=1, new_value="Tesla Starlink")
+    # q.delete_item(1)
+    # q.retrieve_all_item()
+    # items_el.DDL.create_all_tables()
+    # items_el.DDL.drop_all_tables()
+    # items_el.DDL.drop_craveditem()
+    # items_el.DDL.drop_item()
+    q.show_all_tables()
 
         
