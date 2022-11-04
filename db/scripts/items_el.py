df5fd792b5dc1af5ef2e0611cbfaf1f94f83f38a
spaceofmiah <agbonzeosazuwa@gmail.com>
2022-11-04 11:46:41 +0100

    Add staticmethod to update and delete record

--- db/scripts/items_el.py
+++ db/scripts/items_el.py
@@ -4,7 +4,8 @@ database table model with SQLAlchemy
 expression language
 """
 from typing import List, Dict
-from sqlalchemy.sql import select
+from sqlalchemy import Integer
+from sqlalchemy.sql import select, bindparam
 
 from db.core.initializer import engine, create_connection
 from db.models.items import table_meta, Item, CravedItem
@@ -54,6 +55,7 @@ class DQL:
 			result = conn.execute(statement)
 			return result
 
+
 class DML:
 	"""Encapsulates database manipulation language (DML)"""
 
@@ -65,11 +67,51 @@ class DML:
 			result = conn.execute(statement)
 			conn.commit()
 
+
 	@staticmethod
 	def add_items(payload:List[Dict[str, str]]):
-		"""Inserts multiple items to Item table"""
+		"""Inserts multiple items to Item table
+		
+		- payload <list> new data to be added to
+						 Item table. Each dict has
+						 key mapped to the Item table
+						 and it's corresponding value.
+		"""
 		with create_connection() as conn:
 			conn.execute(Item.insert(), payload)
+			conn.commit()
+
+	@staticmethod
+	def delete_item(item_id:int):
+		"""Deletes an item whose id is passed as a 
+		parameter
+
+		- item_id <int> Uniquely identifies an item
+						instance
+		"""
+		with create_connection() as conn:
+			statement = Item.delete().where(
+				Item.c.id==bindparam('id', type_=Integer)
+			)
+			conn.execute(statement, {'id': item_id})
+			conn.commit()
+
+	@staticmethod
+	def update_item(item_id:int, data:Dict[str, str]):
+		"""Updates an existing item
+
+		- item_id <int> Uniquely identifies an item
+						instance
+
+		- data <dict> 	Key-value pair with column name
+						as key and the new entry for 
+						column as value.
+		"""
+		with create_connection() as conn:
+			statement=Item.update().where(
+				Item.c.id==bindparam('item_id', type_=Integer)
+			).values(**data)
+			conn.execute(statement, {'item_id': item_id})
 			conn.commit()
 
 
